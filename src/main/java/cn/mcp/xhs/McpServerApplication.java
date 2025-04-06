493da6b277f972f51e2e02ef1d0b6770da5c989a
Zexu Lin <1664289871@qq.com>
2025-04-06 13:33:56 +0800

    feat: init project

---
+++ src/main/java/cn/mcp/xhs/McpServerApplication.java
@@ -0,0 +1,14 @@
+package cn.mcp.xhs;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linzexu
+ * @version 1.0
+ * @description: TODO
+ * @date 2025/4/6 13:09
+ */
+
+@SpringBootApplication
+public class McpServerApplication {
+}
---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author linzexu
+ * @version 1.0
+ * @description: TODO
+ * @date 2025/4/6 13:13
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+    private final Logger log = LoggerFactory.getLogger(ApiTest.class);
+}
